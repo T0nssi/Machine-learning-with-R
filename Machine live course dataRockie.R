f702bcc6f5f7f9fac1dd2d43621ae5c1b5881d5d
T0nssi <105599410+T0nssi@users.noreply.github.com>
2023-09-10 22:04:16 +0700

    Final and HW

--- Machine live course dataRockie.R
+++ Machine live course dataRockie.R
@@ -22,9 +22,38 @@ train_df<-clean_df[train_id,]
 test_df<-clean_df[-train_id,]
 ##data freshness->fast changing data set/behavior
 
+split_data<-function(df){
+  set.seed(42)
+  n<-nrow(df)
+  train_id<-sample(1:n,size=0.8*n)#add % train if data has bigger size
+  train_df<-df[train_id,]
+  test_df<-df[-train_id,]
+  return(list(train_df,test_df))
+}
 
+#2 train model
+lm_model<-train(mpg~.,
+      data=train_df,
+      method="lm")#change model to increase ability of prediction, predict-evaluaton may be reduce
 
+lm_model<-train(mpg~.,
+                data=train_df,
+                method="ranger")
 
+#3 score
+p<-predict(lm_model,newdata = test_df)
+
+#4 evaluation
+## mean absolute error
+mae<-mean(abs(p-test_df$mpg))
+## root mean absolute error
+rmae<-sqrt(mean(abs(p-test_df$mpg)**2))
+
+lm_model$finalModel
+varImp(lm_model)
+
+# HW create prediction form regression model by dataworld
+https://data.world/dataindianset2000/house-price-india
 
 
 
